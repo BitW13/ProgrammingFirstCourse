ecb3ecd43babad565a2e6c36da7de1277f90fceb
Artsiom Shabolda <Artsiom_Shabolda@epam.com>
2020-01-04 16:41:10 +0300

    Lab 7 is completed

---
+++ Lab7/Lab7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 1;
+
+            const double E = 0.0001;
+
+            int i = 1;
+
+            while(sum.ToString().Length < E.ToString().Length)
+            {
+                sum += 1 / (Math.Pow(2, i));
+
+                i++;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
