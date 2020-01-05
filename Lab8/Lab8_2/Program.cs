771902b850ca5a3325ea51a38c09330f62919f65
Artsiom Shabolda <Artsiom_Shabolda@epam.com>
2020-01-05 21:59:09 +0300

    Lab 8 is completed

---
+++ Lab8/Lab8_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("x min:");
+            double xMin = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("x max:");
+            double xMax = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("dx:");
+            double dX = Convert.ToDouble(Console.ReadLine());
+
+            double e = 0.0001;
+
+            for(double x = xMin; x<=xMax; x += dX)
+            {
+                double sum = 1/x;
+                int n = 1;
+                while ((2*sum).ToString().Length < e.ToString().Length)
+                {
+                    sum += 1 / ((2 * n + 1) * Math.Pow(x, 2 * n + 1));
+                    n++;
+                }
+
+                Console.WriteLine($"{x} ---  {2*sum} --- {n}");
+            }
+        }
+    }
+}
