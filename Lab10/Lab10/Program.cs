82f66757e4568629b3c23f7f44409ef6558dddda
Artsiom Shabolda <shabolda.artem@mail.ru>
2020-01-06 04:11:41 +0300

    lab 10 is completed

---
+++ Lab10/Lab10/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static int IndexOfNegativeNumber(double[] array, int startIndex)
+        {
+            int index = -1;
+
+            for(int i = startIndex; i<array.Length; i++)
+            {
+                if(array[i] < 0)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        }
+
+        static double GetSumNumbersBetweenNegativeNumbers(double[] array)
+        {
+            double result = 0;
+
+            int index = IndexOfNegativeNumber(array, 0);
+
+            if (index == -1)
+            {
+                return result;
+            }
+
+            int endIndex = IndexOfNegativeNumber(array, index + 1);
+
+            if(endIndex == -1)
+            {
+                endIndex = array.Length;
+            }
+
+            for (int i = index + 1; i < endIndex; i++)
+            {
+                result += array[i];
+            }
+
+            return result;
+        }
+
+        static double[] Sort(double[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (Math.Abs(array[i]) > Math.Abs(array[j]))
+                    {
+                        double temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+
+            return array;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("length of array:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            double[] array = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(array.Min());
+            Console.WriteLine(GetSumNumbersBetweenNegativeNumbers(array));
+
+            array = Sort(array);
+
+            foreach (double item in array)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
