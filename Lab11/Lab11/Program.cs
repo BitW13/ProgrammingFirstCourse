7115c8f7bdc036087c32e7fc005c61213093a221
Artsiom Shabolda <shabolda.artem@mail.ru>
2020-01-08 02:57:52 +0300

    Настя прости))

---
+++ Lab11/Lab11/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("rows:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("columns:");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            int[,] array = new int[n, m];
+
+            for(int i =0; i< n; i++)
+            {
+                for(int j = 0; j< m; j++)
+                {
+                    array[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            for(int i = 0; i< n; i++)
+            {
+                bool isPositive = true;
+
+                for(int j =0; j< m; j++)
+                {
+                    if(array[i,j] < 0)
+                    {
+                        isPositive = false;
+                        break;
+                    }
+                }
+
+                if (isPositive)
+                {
+                    int result = 1;
+
+                    for(int j = 0; j< m; j++)
+                    {
+                        result *= array[i, j];
+                    }
+
+                    Console.WriteLine($"{i} |----| {result}");
+                }                
+            }
+        }
+    }
+}
---
+++ Lab11/Lab11_2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11_2
+{
+    class Program
+    {
+        static int GetNumberOfRowContainsZero(int[,] array, int rows, int columns)
+        {
+            int result = 0;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (array[i, j] == 0)
+                    {
+                        result++;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        static int GetNumberOfRowContainsMostNumberOfSameValue(int[,] array, int rows, int columns)
+        {
+            int index = -1;
+            int numberForAllRows = 0;
+
+            for(int i = 0; i< rows; i++)
+            {
+                int value = array[i,0];
+                int number = 0;
+
+                for(int j = 0; j< columns; j++)
+                {
+                    if(array[i,j] == value)
+                    {
+                        number++;
+                    }
+                    else
+                    {
+                        if(number > numberForAllRows)
+                        {
+                            numberForAllRows = number;
+                            index = i;
+                        }
+
+                        number = 0;
+                    }
+
+                    value = array[i, j];
+                }
+            }
+
+            return index;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("m:");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            int[,] array = new int[n, m];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    array[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine(GetNumberOfRowContainsZero(array, n, m));
+
+            Console.WriteLine(GetNumberOfRowContainsMostNumberOfSameValue(array, n, m));
+        }
+    }
+}
